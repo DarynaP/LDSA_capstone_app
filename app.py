7221ee2a1cb0d61667030a9e4da479fe32fc452e
DarynaP <darynaa@hotmail.com>
2023-04-28 17:21:30 +0100

    tests

--- app.py
+++ app.py
@@ -136,15 +136,14 @@ def should_search():
     p = Prediction(
         observation_id = _id,
         outcome = predicted,
-        observation = obs_dict
-    )
+        observation = obs_dict)
     try:
         p.save()
     except IntegrityError:
-        #error_msg = "ERROR: ID: '{}' already exists".format(_id)
-        #response = {'error': error_msg}
+        error_msg = "ERROR: ID: '{}' already exists".format(_id)
+        response = {'error': error_msg}
         DB.rollback()
-    #return jsonify(response)
+    return jsonify(response)
 
 @app.route('/search_result/', methods=['POST'])
 def search_result():
